52d2ca28acd39f17d71b01e93ff1f44917c56775
joaohnt <jpedrooo.cg@gmail.com>
2024-11-23 12:05:26 -0300

    project created

---
+++ src/main/java/com/joaozada/bunniesclub/BunniesClubApplication.java
@@ -0,0 +1,13 @@
+package com.joaozada.bunniesclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BunniesClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BunniesClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaozada/bunniesclub/BunniesClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaozada.bunniesclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunniesClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
